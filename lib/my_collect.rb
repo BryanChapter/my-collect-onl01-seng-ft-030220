35c36c234fd8040f6a12e305949294708cef6686
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-09 16:57:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,8 @@
 def my_collect(language)
   i = 0
+  language=[]
 while i < language.length {|lang| lang.upcase}
   yield (language[i])
-  i+=1
-  end
-  language
-end
-
-def my_collect(language)
-  i = 0
-while i < language.length {|lang| lang.upcase}
-  yield (language.first[i])
   i+=1
   end
   language
